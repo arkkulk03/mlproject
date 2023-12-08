8a4b23050c5c8488bdfd7d8546b57488f4c76903
Aditya kulkarni <152088700+arkkulk03@users.noreply.github.com>
2023-12-08 12:45:16 -0500

    nre requ

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-from setuptools import find_packages,setup
-
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e.'
-def get_requirements(fule_path:str)->list[str]:
+HYPEN_E_DOT = '-e .'
 
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
-requirements=[]
-with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace ("\n","")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            # Handle -e . separately, you may want to skip it for setup.py
+            requirements.remove(HYPEN_E_DOT)
 
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
-return requirements   
+    return requirements
 
 setup(
-name='mlproject,
-version='o.o.1',
-author='Aditya'
-author='aditya.kulkarni200@gmail.com'
-packages=find_packages()
-install_requires= get_requirements('requiremants.txt')
-
-
+    name='ml',
+    version='0.0.1',
+    author='Aditya',
+    author_email='aditya.kulkarni200@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
